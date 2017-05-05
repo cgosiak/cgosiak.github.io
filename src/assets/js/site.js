6a03af29a88dc6ca5f2b243072c553c143c1762e
Caleb Gosiak <cgosiak24@gmail.com>
2017-05-05 11:25:14 -0500

    Building Github API Integration

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -1,13 +1,28 @@
 $(document).ready(function () {
 
-	// Link in MDL Drawer is Clicked
-    $('.drawer_link').click(function() {
-		closeMDLDrawer();
-	});
+  getProjects();
 
-	function closeMDLDrawer() {
-		$('.mdl-layout__drawer').toggleClass('is-visible');
-		$('.mdl-layout__obfuscator').toggleClass('is-visible');
-	}
+  // Link in MDL Drawer is Clicked
+  $('.drawer_link').click(function () {
+    closeMDLDrawer();
+  });
+
+  function closeMDLDrawer() {
+    $('.mdl-layout__drawer').toggleClass('is-visible');
+    $('.mdl-layout__obfuscator').toggleClass('is-visible');
+  }
+
+  function getProjects() {
+    $.ajax({
+      type: "GET",
+      url: "https://api.github.com/users/cgosiak/repos",
+      async: true,
+      success: function (response) {
+		for (i=0; i < response.length; i++) {
+			console.log("Object: " + JSON.stringify(response[i].name));
+		}
+      }
+    });
+  }
 
 });
